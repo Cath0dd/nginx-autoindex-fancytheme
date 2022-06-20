0a5b25eb489e461f6ec53aafa7ec974de1300f8a
Cath0dd <Cath0dd@users.noreply.github.com>
2022-06-20 14:22:42 +0300

    fix

--- .indexer/search.js
+++ .indexer/search.js
@@ -1,40 +1,35 @@
-let form = document.createElement('form');
-let input = document.createElement('input');
-let btn = document.createElement("button");
-
+let form = document.createElement("form"),
+    input = document.createElement("input"),
+    btn = document.createElement("button");
 btn.innerHTML = "Clear";
 btn.id = "clear";
-
-input.name = 'filter';
-input.id = 'search';
-input.placeholder = 'Type to search...';
-
+input.name = "filter";
+input.id = "search";
+input.placeholder = "Type to search...";
 form.appendChild(input);
 form.appendChild(btn);
-
-document.querySelector('h1').after(form);
-
-let listItems = [].slice.call(document.querySelectorAll('#list tbody tr'));
-
-input.addEventListener('keyup', function () {
-    let i,
-    e = "(^|.*[^\\pL])" + this.value.trim().split(/\s+/).join("([^\\pL]|[^\\pL].*[^\\pL])") + ".*$",
-    n = RegExp(e, "i");
-    listItems.forEach(function(item) {
-        item.removeAttribute('hidden');
+document.querySelector("h1").after(form);
+let listItems = [].slice.call(document.querySelectorAll("#list tbody tr"));
+input.addEventListener("keyup", function () {
+    let b,
+        c = "(^|.*[^\\pL])" + this.value.trim().split(/\s+/).join("([^\\pL]|[^\\pL].*[^\\pL])") + ".*$",
+        d = RegExp(c, "i");
+    listItems.forEach(function (a) {
+        a.removeAttribute("hidden");
     });
-    listItems.filter(function(item) {
-        i = item.querySelector('td').textContent.replace(/\s+/g, " ");
-        return !n.test(i);
-    }).forEach(function(item) {
-  	    item.hidden = true;
-    });
+    listItems
+        .filter(function (a) {
+            b = a.querySelector("td").textContent.replace(/\s+/g, " ");
+            return !d.test(b);
+        })
+        .forEach(function (a) {
+            a.hidden = !0;
+        });
 });
-
-btn.addEventListener('click', evt => {
-    evt.preventDefault();
-    input.value = '';
-    listItems.forEach(function(item) {
-        item.removeAttribute('hidden');
+btn.addEventListener("click", (b) => {
+    b.preventDefault();
+    input.value = "";
+    listItems.forEach(function (c) {
+        c.removeAttribute("hidden");
     });
 });
